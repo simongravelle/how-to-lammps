a8a7a0723fd98223dc7eca3f913a29154b73aab3
Simon Gravelle <sgravelle@icp.uni-stuttgart.de>
2022-09-16 15:01:24 +0200

    deleted vscode

--- colorise.sh
+++ colorise.sh
@@ -2,29 +2,28 @@
 
 while IFS="" read -r folder || [ -n "$folder" ]
 do
-
+    if [ -f $folder'/'$folder'.rst' ] ; then
+        rm $folder'/'$folder'.rst'
+    fi
     touch $folder'/'$folder'.rst'
-
     while IFS="" read -r line || [ -n "$line" ]
     do
-        #echo $line
-
         if [[ $line = " "* ]]; then
-            newline=''
-            for val in $line
-            do   
-                if grep -Fxq $val lammps.constants
-                then 
-                    newline=$newline' '$val 
-                else 
-                    newline=$newline' '$val 
-                fi
-            done
-            echo $newline
+            #newline=''
+            #for val in $line
+            #do   
+            #    if grep -Fxq $val lammps.constants
+            #    then 
+            #        newline=$newline' '$val 
+            #    else 
+            #        newline=$newline' '$val 
+            #    fi
+            #done
+            #echo $newline
+            #echo $newline >> $folder'/'$folder'.rst'
+            echo '    '$line >> $folder'/'$folder'.rst'
         else
             echo $line >> $folder'/'$folder'.rst'
         fi
-
-
     done < $folder'/raw-rst/'$folder'.rst'
 done < folderlist.txt
